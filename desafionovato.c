99d13f8fe32b5a86c3be4a4276f15b046d5c9a02
shelena11 <shelena11@libero.it>
2025-05-21 13:00:56 +0000

    fiz as duas cartas 1 e 2

--- desafionovato.c
+++ desafionovato.c
@@ -9,7 +9,7 @@
     //carta 1
 
 int main(){
-    char estado[50]= "Pernambuco";
+    char estado[30]= "Pernambuco";
     char codigo [20]= "A01, A02, A03, A04";
     char cidade [20]= "Recife";
     float populacao= 1489.000;
@@ -17,7 +17,7 @@ int main(){
     float pib= 33.000000;
     int turistico= 30;
 
-    printf("O Estado é: %s\n", estado);
+    printf("O estado é: %s\n", estado);
     printf("O codigo é: %s\n", codigo);
     printf("A cidade é: %s\n", cidade);
     printf("A populacao é: %f\n", populacao);
@@ -35,13 +35,13 @@ int main(){
     float Pib= 27.000000;
     int Turistico= 15;
 
-    printf("O Estado é: %s\n", estado);
-    printf("O Codigo é: %s\n", codigo);
-    printf("A Cidade é: %s\n", cidade);
-    printf("A Populacao é: %f\n", populacao);
-    printf("A Area é: %f\n", area);
-    printf("O Pib é: %f\n", pib);
-    printf("O Turistico é: %d\n", turistico);
+    printf("O Estado é: %s\n", Estado);
+    printf("O Codigo é: %s\n", Codigo);
+    printf("A Cidade é: %s\n", Cidade);
+    printf("A Populacao é: %f\n", Populacao);
+    printf("A Area é: %f\n", Area);
+    printf("O Pib é: %f\n", Pib);
+    printf("O Turistico é: %d\n", Turistico);
 
 
     return 0;
