4fa06498518da3044f1be1e430a9aaac84c869b4
shelena11 <shelena11@libero.it>
2025-05-21 11:03:20 +0000

    fiz carta 1 recife

--- desafionovato.c
+++ desafionovato.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 
+    //Desafio super Trunfo 
+    //Desafio novato
+    //Tema Paises
+    //Cadastrar cartas de cidades
+    //Teste Helena
+
 int main(){
-    printf("Desafio super Trunfo novato\n");
-    
+    char estado[50]= "Pernambuco";
+    char codigo [20]= "A01, A02, A03, A04";
+    char cidade [20]= "Recife";
+    float populacao= 1489.000;
+    float area= 218.843;
+    float pib= 33.000000;
+    int turistico= 30;
+
+    printf("O Estado é: %s\n", estado);
+    printf("O codigo é: %s\n", codigo);
+    printf("A cidade é: %s\n", cidade);
+    printf("A populacao é: %f\n", populacao);
+    printf("A area é: %f\n", area);
+    printf("O pib é: %f\n", pib);
+    printf("O turistico é: %d\n", turistico);
+
+
+    return 0;
 
 
 }
