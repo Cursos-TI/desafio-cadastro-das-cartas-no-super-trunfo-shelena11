14ba3f3fc4941612d715a81db7f23949ce76ece5
shelena11 <shelena11@libero.it>
2025-05-21 12:40:42 +0000

    fiz carta 2

--- desafionovato.c
+++ desafionovato.c
@@ -6,6 +6,8 @@
     //Cadastrar cartas de cidades
     //Teste Helena
 
+    //carta 1
+
 int main(){
     char estado[50]= "Pernambuco";
     char codigo [20]= "A01, A02, A03, A04";
@@ -22,6 +24,24 @@ int main(){
     printf("A area é: %f\n", area);
     printf("O pib é: %f\n", pib);
     printf("O turistico é: %d\n", turistico);
+
+    //carta 2
+
+    char Estado[30]= "Ceara";
+    char Codigo [20]= "B01, B02, B03, B04";
+    char Cidade [20]= "Fortaleza";
+    float Populacao= 4284.450;
+    float Area= 313.800;
+    float Pib= 27.000000;
+    int Turistico= 15;
+
+    printf("O Estado é: %s\n", estado);
+    printf("O Codigo é: %s\n", codigo);
+    printf("A Cidade é: %s\n", cidade);
+    printf("A Populacao é: %f\n", populacao);
+    printf("A Area é: %f\n", area);
+    printf("O Pib é: %f\n", pib);
+    printf("O Turistico é: %d\n", turistico);
 
 
     return 0;
