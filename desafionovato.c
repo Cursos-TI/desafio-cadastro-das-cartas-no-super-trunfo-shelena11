92ae735b0bc77f88229b94b27a6ae6eeef4bc45a
shelena11 <shelena11@libero.it>
2025-05-22 21:33:53 +0000

    fiz o teste e deu certo com sprintf e scanf

--- desafionovato.c
+++ desafionovato.c
@@ -15,7 +15,7 @@ int main(){
     float populacao= 1489.000;
     float area= 218.843;
     float pib= 33.000000;
-    int numero ponto turistico= 30;
+    int turistico = 30;
 
     printf("Entre com o estado:\n");
     scanf("%s", &populacao);
@@ -24,13 +24,13 @@ int main(){
     printf("entre com a cidade:\n");
     scanf("%s", &codigo);
     printf("entre com a populacao:\n");
-    scanf("%.2f", &populacao);
+    scanf("%f", &populacao);
     printf("entre com a area:\n");
-    scanf("%.2f", &area);
+    scanf("%f", &area);
     printf("entre com o pib:\n");
-    scanf("%.2f", &pib);
+    scanf("%f", &pib);
     printf("entre com turistico:\n ");
-    scanf("%.2f", &turistico);
+    scanf("%f", &turistico);
 
     printf("O estado é: %s\n", estado);
     printf("O codigo é: %s\n", codigo);
@@ -40,15 +40,32 @@ int main(){
     printf("O pib é: %f\n", pib);
     printf("O turistico é: %d\n", turistico);
 
+
+    
     //carta 2
 
     char Estado[30]= "Ceara";
-    char Codigo [20]= "B01, B02, B03, B04";
+    char Codigo [20]= "B01";
     char Cidade [20]= "Fortaleza";
     float Populacao= 4284.450;
     float Area= 313.800;
     float Pib= 27.000000;
     int Turistico= 15;
+
+    printf("Entre com o estado:\n");
+    scanf("%s", &populacao);
+    printf("entre com o codigo:\n");
+    scanf("%s",&codigo);
+    printf("entre com a cidade:\n");
+    scanf("%s", &codigo);
+    printf("entre com a populacao:\n");
+    scanf("%f", &populacao);
+    printf("entre com a area:\n");
+    scanf("%f", &area);
+    printf("entre com o pib:\n");
+    scanf("%f", &pib);
+    printf("entre com turistico:\n ");
+    scanf("%f", &turistico);
 
     printf("O Estado é: %s\n", Estado);
     printf("O Codigo é: %s\n", Codigo);
